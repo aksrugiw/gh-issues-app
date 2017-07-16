f037e0695c999078c55f2057a54d9f7f17cd883c
aksrugiw <aksrugiw@interia.pl>
2017-07-16 13:38:00 +0200

    Create issue service, fetch issues, show issues list

--- src/app/issue/issue.component.ts
+++ src/app/issue/issue.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { IssueService } from './issue.service'
 
 @Component({
   selector: 'issue',
   templateUrl: './issue.component.html',
-  styleUrls: ['./issue.component.css']
+  styleUrls: ['./issue.component.css'],
+  providers: [IssueService]
 })
 export class IssueComponent implements OnInit {
+  private issues: any;
 
-  constructor() { }
+  constructor(private _issueService: IssueService) { }
 
   ngOnInit() {
+    this._issueService.getAllIssues()
+      .subscribe(issues => {
+        this.issues = issues;
+      });
   }
 
 }
---
+++ src/app/issue/issue.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class IssueService {
+  private _baseUrl = 'https://api.github.com/repos/angular/angular/issues';
+
+  constructor(private _http: Http) { }
+
+  getAllIssues() {
+    return this._http.get(this._baseUrl)
+      .map(res => res.json());
+  }
+
+}
