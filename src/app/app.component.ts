286f6e789e6d8751b07e4e3e860b39267daf702a
aksrugiw <aksrugiw@interia.pl>
2017-07-16 15:54:28 +0200

    Add issue service to root component, display isssues length

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IssueService } from './issue/issue.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [IssueService]
+
 })
-export class AppComponent {
-  private currentType = 'all';
+export class AppComponent implements OnInit {
+  private currentType = '';
+  private allIssues = [];
+  private openIssues = [];
+  private closedIssues = [];
+  private displayedIssues = [];
+
+  constructor(private _issueService: IssueService) { }
+
+  ngOnInit() {
+    this._issueService.getAllIssues()
+        .subscribe(issues => {
+          this.allIssues = issues;
+          this.displayedIssues = issues;
+        });
+    this._issueService.getOpenIssues()
+        .subscribe(issues => {
+          this.openIssues = issues;
+          // console.log(this.openIssues.length);
+        });
+    this._issueService.getClosedIssues()
+        .subscribe(issues => {
+          this.closedIssues = issues;
+        });
+  }
 
   filterBy(type) {
     this.currentType = type;
+    
+    if (type === 'all') {
+      this.displayedIssues = this.allIssues;
+    }
+    else if (type === 'open') {
+      this.displayedIssues = this.openIssues;
+    }
+    else if (type === 'closed') {
+      this.displayedIssues = this.closedIssues;
+    }
   }
 }
--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -1,48 +1,10 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import { IssueService } from '../issue/issue.service'
 
 @Component({
   selector: 'issue-list',
   templateUrl: './issue-list.component.html',
-  styleUrls: ['./issue-list.component.css'],
-  providers: [IssueService]
-
+  styleUrls: ['./issue-list.component.css']
 })
-export class IssueListComponent implements OnInit, OnChanges {
-  private allIssues: any;
-  private openIssues: any;
-  private closedIssues: any;
-  private displayedIssues: any;
-  @Input() issueType = '';
-
-  constructor(private _issueService: IssueService) { }
-
-  ngOnInit() {
-    this._issueService.getAllIssues()
-        .subscribe(issues => {
-          this.allIssues = issues;
-          this.displayedIssues = issues;
-        });
-    this._issueService.getOpenIssues()
-        .subscribe(issues => {
-          this.openIssues = issues;
-        });
-    this._issueService.getClosedIssues()
-        .subscribe(issues => {
-          this.closedIssues = issues;
-        });
-  }
-
-  ngOnChanges() {
-    if (this.issueType === 'all') {
-      this.displayedIssues = this.allIssues;
-    }
-    else if (this.issueType === 'open') {
-      this.displayedIssues = this.openIssues;
-    }
-    else if (this.issueType === 'closed') {
-      this.displayedIssues = this.closedIssues;
-    }
-  }
-
+export class IssueListComponent  {
+  @Input() issues;
 }
