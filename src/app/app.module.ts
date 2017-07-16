991d88ceff54bf42ce5fa632d531766b71fb343f
aksrugiw <aksrugiw@interia.pl>
2017-07-16 12:41:07 +0200

    Create issue-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
 import { IssueComponent } from './issue/issue.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    IssueListComponent,
     IssueComponent
   ],
   imports: [
---
+++ src/app/issue-list/issue-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'issue-list',
+  templateUrl: './issue-list.component.html',
+  styleUrls: ['./issue-list.component.css']
+})
+export class IssueListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
