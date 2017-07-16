55d0ae1abd34e83c0e89c1bda96212b5a34712e3
aksrugiw <aksrugiw@interia.pl>
2017-07-16 12:30:38 +0200

    Add issue component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { IssueComponent } from './issue/issue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueComponent
   ],
   imports: [
     BrowserModule,
